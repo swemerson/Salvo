ea5bba45242b4063efe2754578c95086add195bf
Sean <swemerson@gmail.com>
2015-11-25 03:03:02 -0800

    Added new scene (Level1) with player
    Player rotation toward mouse and movement implemented.

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour 
+{
+	public Button startButton;
+
+	void Start()
+	{
+		startButton = startButton.GetComponent<Button>();
+	}
+
+	public void StartPress()
+	{
+		Application.LoadLevel(1);
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour 
+{
+	private Vector3 movement;
+	private float speed;
+	private Rigidbody2D rb2d;
+
+	void Start () 
+	{
+		speed = 15f;
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	void Update () 
+	{
+		// Rotate player to mouse position
+		Vector3 mousePos = Input.mousePosition;
+		Vector3 objectPos = Camera.main.WorldToScreenPoint (transform.position);
+		mousePos.x = mousePos.x - objectPos.x;
+		mousePos.y = mousePos.y - objectPos.y;	
+		float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+		// Movement
+		float x = Input.GetAxisRaw("Horizontal");
+		float y = Input.GetAxisRaw ("Vertical");
+		movement.Set(x, y, 0f);
+		movement = movement.normalized * speed * Time.deltaTime;
+		rb2d.MovePosition(transform.position + movement);
+	}
+}
