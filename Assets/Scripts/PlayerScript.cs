300ed0f0d86cc316e3a82be27b408129abb82c6e
Sean <swemerson@gmail.com>
2015-12-02 04:17:15 -0800

    Player can now shoot!

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,14 +3,20 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour 
 {
+	public GameObject bullet;
+	public Transform bulletSpawn;
+	public float fireRate;
+
 	private Vector3 movement;
 	private float speed;
 	private Rigidbody2D rb2d;
+	private float nextFire;
 
 	void Start () 
 	{
 		speed = 35f;
 		rb2d = GetComponent<Rigidbody2D>();
+		fireRate = 0.07f;
 	}
 
 	void Update () 
@@ -23,11 +29,18 @@ public class PlayerScript : MonoBehaviour
 		float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
 		transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 
-		// Movement
+		// Movement (WASD or Arrows)
 		float x = Input.GetAxisRaw("Horizontal");
 		float y = Input.GetAxisRaw ("Vertical");
 		movement.Set(x, y, 0f);
-		movement = movement.normalized * speed * Time.deltaTime;
+		movement = movement.normalized * speed * Time.smoothDeltaTime;
 		rb2d.MovePosition(transform.position + movement);
+
+		// Shoot (Mouse 1)
+		if (Input.GetButton("Fire1") && Time.time > nextFire) 
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+		}
 	}
 }
